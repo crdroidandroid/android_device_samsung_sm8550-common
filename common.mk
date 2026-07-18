337ce204a2dd0830309aabd03881dbc9f204b84a
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-07-18 20:57:35 +0200

    sm8550-common: Include Sony Dolby Atmos * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby [1]: sm8550-common: Add dolby volume listener effect [2]: sm8550-common: audio: Bring all stream effects
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Change-Id: I0c7bd2ee8405555fc2ed48a2bd854c1f57468234
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -135,6 +135,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
+# Lunaris Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
