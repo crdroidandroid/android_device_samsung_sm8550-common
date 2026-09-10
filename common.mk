8236204c9fa7880f32d25db5a1c7aff70118e6f6
Mathias Gluszczynski <admin@krazey.de>
2026-09-10 13:06:08 +0200

    sm8550: Enable VoIP sample-rate calibration
    Enable the PAL sample-rate calibration option for the VoIP processing
    graphs used by dm1q.
    
    Device logs confirm the 16 kHz rate key on speaker and earpiece
    capture, and the user reports resolved VoWiFi speakerphone echo.
    
    Change-Id: I7e0549b71611025b5747b175845172f4fe4fdd2a

--- common.mk
+++ common.mk
@@ -21,6 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Audio
+$(call soong_config_set_bool,qtiaudio,pal_voip_sample_rate_calibration,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl.samsung-sm8550 \
     android.hardware.audio.effect@7.0-impl \
